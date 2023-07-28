7a61b316a624557cd1606e25cec23d14d6f00077
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2023-07-28 19:02:59 +0500

    Update database.js

--- all-about-next-js/utils/database.js
+++ all-about-next-js/utils/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const { connection } = await mongoose.connect(
-      "mongodb+srv://farrukhadeel:pps993icp956@cluster0.ysuiv.mongodb.net/all-about-nextjs"
+      "mongo db url"
     );
     console.log(`Database connected on ${connection.host}`);
   } catch (error) {
